42aaf09422ee5064ddc5cb984061c419118477be
Arialo <71202311+Arialo@users.noreply.github.com>
2021-06-12 04:36:47 -0500

    Update Arch-install-script-pt2.sh

--- Arch-install-script-pt2.sh
+++ Arch-install-script-pt2.sh
@@ -6,11 +6,11 @@ echo archtest > /etc/hostname
 touch /etc/hosts
 echo 127.0.0.1	localhost ::1		localhost 127.0.1.1	archtest > /etc/hosts
 passwd
-pacman -S grub
+pacman -S grub -y
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 pacman -S xorg xfce4 xfce4-goodies firefox sddm NetworkManager -y
-systemctl enable lightdm.service
+systemctl enable sddm.service
 systemctl enable NetworkManager.service
 useradd example
 exit
