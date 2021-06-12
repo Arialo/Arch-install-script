609150fc9407d40f3abc82d637062efff0727046
Arialo <71202311+Arialo@users.noreply.github.com>
2021-06-12 04:16:31 -0400

    Update Arch-install-script.sh

--- Arch-install-script.sh
+++ Arch-install-script.sh
@@ -1,5 +1,5 @@
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
-pacstrap /mnt base linux linux-firmware nano build make fakeroot
+pacstrap /mnt base linux linux-firmware nano make fakeroot
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt << "sh Arch-install-script-pt2.sh"
