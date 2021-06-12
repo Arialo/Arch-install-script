f8ecefa62af7c5ff3cda5a3645a6933f83b7a0ab
Arialo <71202311+Arialo@users.noreply.github.com>
2021-06-12 04:15:41 -0400

    Create Arch-install-script-pt2.sh

---
+++ Arch-install-script-pt2.sh
@@ -0,0 +1,18 @@
+timedatectl set-timezone America/Chicago
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+export LANG=en_US.UTF-8
+echo archtest > /etc/hostname
+touch /etc/hosts
+echo 127.0.0.1	localhost ::1		localhost 127.0.1.1	archtest > /etc/hosts
+passwd
+arch
+pacman -S grub
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S xorg xfce4 xfce4-goodies firefox lightdm NetworkManager
+systemctl start lightdm.service
+systemctl enable lightdm.service
+systemctl enable NetworkManager.service
+exit
+shutdown now
