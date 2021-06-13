6858f7933023969d3826ecff4d1bd19af2ca830c
Arialo <71202311+Arialo@users.noreply.github.com>
2021-06-13 07:09:35 -0400

    Update Arch-install-script-pt2.sh

--- Arch-install-script-pt2.sh
+++ Arch-install-script-pt2.sh
@@ -5,14 +5,17 @@ export LANG=en_US.UTF-8
 echo archtest > /etc/hostname
 touch /etc/hosts
 echo 127.0.0.1	localhost ::1		localhost 127.0.1.1	archtest > /etc/hosts
+echo "Set the password for the root user"
 passwd
 pacman -S grub -y
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
-pacman -S xorg xfce4 xfce4-goodies firefox -y
+pacman -S xorg xfce4 xfce4-goodies firefox sudo -y
 pacman -S sddm networkmanager -y
 systemctl enable sddm.service
 systemctl enable NetworkManager.service
 useradd example
+echo "Set the password for the base user"
 passwd example
+mkdir /home/example
 exit
