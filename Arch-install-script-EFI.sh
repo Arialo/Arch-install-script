912eb5dd77898c39144567266526d48d548a522f
Arialo <71202311+Arialo@users.noreply.github.com>
2021-12-02 19:25:30 -0600

    Update Arch-install-script-EFI.sh

--- Arch-install-script-EFI.sh
+++ Arch-install-script-EFI.sh
@@ -3,6 +3,6 @@ mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
 pacstrap /mnt base linux linux-firmware nano make fakeroot
 genfstab -U /mnt >> /mnt/etc/fstab
-cp Arch-install-script-pt2.sh /mnt
+cp Arch-install-script-EFI-pt2.sh /mnt
 cp Arch-install-script/.xinitrc /mnt
 arch-chroot /mnt
